6f287b365eb25685c4e816217ff555d1fcc5d302
mihir060708 <114179395+mihir060708@users.noreply.github.com>
2022-09-23 23:45:10 +0530

    Create SumOfNaturalNumber1.java
    Sum of first nutural numbers.

---
+++ SumOfNaturalNumber1.java
@@ -0,0 +1,13 @@
+public class Main  
+{  
+public static void main(String[] args)   
+{  
+int i,num = 50, sum = 0;  
+ 
+for(i = 1; i <= num; ++i)  
+{  
+sum = sum + i;  
+}  
+System.out.println("Sum of First 50 Natural Numbers is = " + sum);  
+}  
+}  
